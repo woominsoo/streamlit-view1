b3f3ade89dc42c4380caac1a3a14085f70712518
woominsoo <154071010+woominsoo@users.noreply.github.com>
2024-11-17 13:02:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,6 @@ import streamlit as st
 import pandas as pd 
 
 view = [100,150,30]
-st.markdown("우민수의 첫 사이트 (소개)")
-st.markdown("웹사이트구성 쉽네?")
-st.markdown('''
-      :red[저의 이름은] :orange[우민수] :green[입니다.] :blue[나이는 13살 이고] :violet[음..]
-    :gray[어...] :rainbow[이상 제 소개였습니다].''')
-
-st.markdown('이런 글씨체도 있네 하하')
-st.markdown('저의 가족 구성원을 ')
-st.markdown('엄마 (곽은정),아빠(우승호),나(우민수),가랑이,가담이(거북이)가 있습니다')
-st.markdown('오늘은 여기까지만 해야겠네요. ')
+st.markdown("사이트 닫음. -2024-11-17-")
 
 
