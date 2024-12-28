a8fb6b936b283ddd5e06caee6b14680d92544926
woominsoo <154071010+woominsoo@users.noreply.github.com>
2024-12-28 11:32:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,4 @@
-from flask import Flask
+import streamlit as st
 
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return """
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>tmdh-business</title>
-    </head>
-    <body>
-        <h1>설명</h1>
-        <p>이 사이트는 간단한 설명을 위한 예제입니다.</p>
-    </body>
-    </html>
-    """
-
-if __name__ == "__main__":
-    app.run(debug=True)
+st.title("tmdh-business")
+st.write("이 사이트는 간단한 설명을 위한 예제입니다.")
