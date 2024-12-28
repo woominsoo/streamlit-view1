45f3e58e797de1991866d28bd22872cfd9a8025b
woominsoo <154071010+woominsoo@users.noreply.github.com>
2024-12-28 11:36:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,22 +3,24 @@ import streamlit as st
 if "page" not in st.session_state:
     st.session_state.page = "home"
 
+st.title("tmdh-business")
+st.write("이 사이트는 간단한 설명을 위한 예제입니다.")
+st.write("")
+
 def home_page():
     st.title("첫 번째 페이지")
-    if st.button("다음 페이지로 이동"):
+    st.title("수제제작 젓가락")
+    st.write("가격 : 59,000WON")
+    if st.button("구매하기"):
         st.session_state.page = "next"
 
 def next_page():
-    st.title("두 번째 페이지")
-    st.write("여기는 두 번째 페이지입니다!")
-    if st.button("첫 번째 페이지로 돌아가기"):
+    st.title("구매페이지")
+    st.write("항목 : ")
+    if st.button("둘러보기"):
         st.session_state.page = "home"
 
 if st.session_state.page == "home":
     home_page()
 elif st.session_state.page == "next":
     next_page()
-
-st.title("tmdh-business")
-st.write("이 사이트는 간단한 설명을 위한 예제입니다.")
-st.write("")
